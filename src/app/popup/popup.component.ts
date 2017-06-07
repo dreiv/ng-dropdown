0c292d820984fdde9e8724d3dd316fe91bda52a5
Andrei Voicu <Andrei-Florian.Voicu@haufe-lexware.com>
2017-06-07 17:32:12 +0300

    made the element lose focus when something else is clicked.

--- src/app/popup/popup.component.ts
+++ src/app/popup/popup.component.ts
@@ -1,15 +1,16 @@
-import { Component, HostBinding, OnInit } from '@angular/core';
+import {Component, ElementRef, HostBinding, HostListener, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-popup',
   templateUrl: './popup.component.html',
-  styleUrls: ['./popup.component.css']
+  styleUrls: ['./popup.component.css'],
 })
 export class PopupComponent implements OnInit {
   @HostBinding('class.open') open;
 
 
-  constructor() { }
+  constructor(private elementRef: ElementRef) {
+  }
 
   ngOnInit() {
   }
@@ -18,4 +19,10 @@ export class PopupComponent implements OnInit {
     this.open = !this.open;
   }
 
+  @HostListener('document:click', ['$event'])
+  onClick(event: Event) {
+    if (!this.elementRef.nativeElement.contains(event.target)) {
+      this.open = false;
+    }
+  }
 }
